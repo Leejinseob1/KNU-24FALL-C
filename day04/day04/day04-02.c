6ebafb25c43996351e41ed6d97addb8204a5bd98
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-09-23 18:04:30 +0900

    포인터 실습

---
+++ day04/day04/day04-02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void swap(int* a1, int* b1)
+{
+	int c = *a1;
+	*a1 = *b1;
+	*b1 = c;
+	return;
+}
+
+int main()
+{
+	int a = 10, b = 10;
+	printf("a= %d, b= &d \n", a, b);
+	swap(&a, &b);
+	printf("a= %d, b= &d \n", a, b);
+	return 0;
+
+}
