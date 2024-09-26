62c0d2f077d0b90d95a657f857136f2269f6a132
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-09-26 14:43:32 +0900

    Create day05-01.c

---
+++ day 05/day 05/day05-01.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+	int N;
+	int* st;
+	char* name;
+	int* score;
+	printf("학생 수 입력: ");
+	scanf_s("%d", &N);
+
+	st= (int*) malloc(sizeof(int) * N);
+	name = (char*)malloc(sizeof(char) * N*100);
+	score = (int*)malloc(sizeof(int) * N);
+
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("학번:");
+		scanf_s("%d", &st[i]);
+
+		printf("이름:");
+		scanf_s("%d", name+(100*i), 100);
+
+		printf("점수:");
+		scanf_s("%d", &score[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+		printf("%d %s %d \n", st[i], name[i], score[i]);
+
+	free(st);
+	free(name);
+	free(score);
+
+	return 0;
+}
