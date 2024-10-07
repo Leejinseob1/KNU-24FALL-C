ac55c4bdbb10037271f6a436f9e1b629a5d228d3
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-10-07 10:21:49 +0900

    반복문

---
+++ day 07/day 07/day 07-01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int n=0;
+	printf("숫자 입력: ");
+	scanf_s("%d", &n);
+
+	for (int i=0; i <= n; i++)
+	{
+		for (int j = n; j > i; j--)
+		{
+			printf(" ");
+		}
+
+		for (int j=0; j < 2*i-1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
