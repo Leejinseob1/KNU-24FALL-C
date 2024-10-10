da968a5044e7d8326f27e42f93a76490c41783e6
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-10-10 14:36:24 +0900

    반복문 실습

---
+++ day07/day07-02.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main()
+{
+
+	double x = 0;
+	double y = 0;
+	double r = 0, pi=0;
+	double count = 0, circle = 0;
+	int i=0, j=0;
+
+	srand(time(NULL));
+// 1,000,000,000 □ ■
+	for (j=0; j <=100; j++)
+	{
+		while (count < 10000000)
+		{
+			x = (double)rand() / (double)RAND_MAX;
+			y = (double)rand() / (double)RAND_MAX;
+
+			if (sqrt(pow(x, 2) + pow(y, 2)) < 1)
+			{
+				circle++;
+			}
+			count++;
+		}
+		r = circle / count * 4;
+		printf("%3d \%진행... 원주율: %lf ", j, r);
+		for (i=0; i < j/5; i++)
+		{
+			printf("■");
+		}
+		for (i=0; i <20- j / 5; i++)
+		{
+			printf("□");
+		}
+		printf("\n");
+		count++;
+		pi =pi+ r;
+	}
+	printf("원주율 =%lf", pi/101);
+	return 0;
+}
