21077000a933f82eeea4c2aa15e8a1d9c56c329a
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-10-16 21:46:23 +0900

    포인터 실습

---
+++ day08/day08-1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+	char sentence[100]= NULL;
+	E:
+
+	printf("문자열을 입력하세요: ");
+	scanf_s("%s", sentence, sizeof(sentence));
+
+	int length = strlen(sentence);
+	char* start = sentence;
+	char* end = sentence + length-1;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (sentence[i]< 65 || sentence[i]>122)
+		{
+			printf("영어만 입력해 주세요. \n");
+			goto E;
+		}
+	}
+	
+	printf("뒤집어진 문자열:");
+
+	while (end >= start)
+	{
+		printf("%c", *end);
+		end--;
+	}
+	return 0;
+}
