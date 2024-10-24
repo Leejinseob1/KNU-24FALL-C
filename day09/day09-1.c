8fd7a59addb5b9a8fc3821928fc92caa70ca135d
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-10-24 10:54:54 +0900

    구조체 실습

---
+++ day09/day09-1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+struct vector1 {
+	int x1;
+	int y1;
+	int z1;
+};
+
+struct vector2 {
+	int x2;
+	int y2;
+	int z2;
+};
+
+
+void Sum_Vector(struct vector1 v1, struct vector2 v2)
+{
+	printf("벡터의 합은 x:%d \ty:%d \tz:%d", v1.x1+v2.x2, v1.y1+v2.y2, v1.z1+v2.z2);
+}
+int Subtraction_Vector(struct vector1 v1, struct vector2 v2)
+{
+	printf("벡터의 차는 x:%d \ty:%d z:%d", v1.x1 - v2.x2, v1.y1-v2.y2, v1.z1-v2.z2);
+}
+
+int CrossProduct_Vector(struct vector1 v1, struct vector2 v2)
+{
+	printf("벡터의 외적은 x:%d \ty:%d \tz:%d", v1.x1 * v2.x2, v1.y1 * v2.y2, v1.z1 * v2.z2);
+
+}
+
+int DotProduvt_Vector(struct vector1 v1, struct vector2 v2)
+{
+	printf("벡터의 내적은 x:%d \ty:%d \tz:%d", v1.y1 * v2.z2 - v1.z1 * v2.y2, v1.z1 * v2.x2-v1.x1*v2.z2, v1.x1*v2.y2-v1.y1*v2.x2);
+
+}
+
+int main()
+{
+	struct vector1 v1;
+	struct vector2 v2;
+	int order=0;
+
+
+	printf("첫 번째 벡터(x,y,z): ");
+	scanf_s("%d, %d, %d", &v1.x1, &v1.y1, &v1.z1);
+	printf("두 번째 벡터(x,y,z): ");
+	scanf_s("%d, %d, %d", &v2.x2, &v2.y2, &v2.z2);
+
+	printf("입력된 첫 번째 벡터 %5d, %5d, %5d\n", v1.x1, v1.y1, v1.z1);
+	printf("입력된 두 번째 벡터 %5d, %5d, %5d\n", v2.x2, v2.y2, v2.z2);
+	printf("\n-------------------------\n");
+
+	printf("1. 벡터의 합\n");
+	printf("2. 벡터의 차\n");
+	printf("3. 벡터의 외적\n");
+	printf("4. 벡터의 내적\n");
+	printf("5. 종료\n");
+	printf("-------------------------\n");
+
+	printf("명령 입력: ");
+	scanf_s("%d", &order);
+
+	if (order == 1)
+		Sum_Vector(v1, v2);
+	else if (order == 2)
+		Subtraction_Vector(v1, v2);
+	else if (order == 3)
+		CrossProduct_Vector(v1, v2);
+	else if (order == 4)
+		DotProduvt_Vector(v1, v2);
+	else
+		printf("잘못된 값");
+
+	return 0;
+}
