3fe41e7b4f7e3d14249c774c41cda7fb2d11593b
CeikawA <161811199+CeikawA@users.noreply.github.com>
2024-10-25 11:49:36 +0900

    Update day09-2.c

--- day09/day09-2.c
+++ day09/day09-2.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Product {
-	int Product_ID[5];
-	char Product_Name[5][30];
-	int Product_Price[5];
+	int Product_ID;
+	char Product_Name[30];
+	int Product_Price;
 };
 
-void printProduct(struct Product p)
+// 상품 정보를 출력하는 함수
+void printProduct(struct Product* p, int count)
 {
-	printf("<<입력된 상품 목록>> \n");
-	while(1)
-	{
-		int j=0;
-		if (p.Product_ID[j] == NULL)
-			break;
-		printf("상품 ID: %d \n", p.Product_ID[j]);
-		printf("상품명: %s \n", p.Product_Name[j]);
-		printf("가격: %d \n", p.Product_Price[j]);
-		printf("\n");
+	printf("<<입력된 상품 목록>>\n");
+	for (int j = 0; j < count; j++) {
+		printf("상품 ID: %d\n", p[j].Product_ID);
+		printf("상품명: %s\n", p[j].Product_Name);
+		printf("가격: %d\n\n", p[j].Product_Price);
 	}
 }
 
 int main()
 {
-	struct Product p[5];
-	// p = (struct Product*)malloc(sizeof(struct Product));
+	struct Product p[5];  // 구조체 배열 선언
 	int i = 0;
-	while(i<5)
+
+	while (i < 5)
 	{
 		printf("상품 정보를 입력하세요 (입력 중단은 ID에 0을 입력)\n");
 		printf("상품 ID: ");
-		scanf_s("%d", &p->Product_ID[i]);
-		if (p->Product_ID[i] == 0)
+		scanf_s("%d", &p[i].Product_ID);
+		if (p[i].Product_ID == 0) {
 			break;
+		}
 		printf("상품명: ");
-		scanf_s("%s", &p->Product_Name[i],30);
+		scanf_s("%s", p[i].Product_Name, 30);
 		printf("가격: ");
-		scanf_s("%d", &p->Product_Price[i]);
+		scanf_s("%d", &p[i].Product_Price);
 		i++;
 	}
 	printf("\n");
 
-	printProduct(p[5]);
+	// 입력된 상품 수만큼 출력
+	printProduct(p, i);
+
 	return 0;
 }
