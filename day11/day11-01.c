296def81aa06ea9ccd3fc0b0fa1af5348866deb7
CeikawA <kt01033884662@gmail.com>
2024-11-24 21:03:12 +0900

    Update day11-01.c

--- day11/day11-01.c
+++ day11/day11-01.c
@@ -5,34 +5,41 @@ int main()
 {
     double a, b, c;
     double discriminant, root1, root2;
-
-    printf("Enter coefficients a, b, and c: ");
-    scanf_s("%lf %lf %lf", &a, &b, &c);
-
-    if (a == 0) 
+    while (1)
     {
-        printf("3차 방정식이 아닙니다.\n");
-        return 1;
-    }
+        printf("이차방정식의 계수를 입력해 주세요. 프로그램을 종료하려면 이차항에 0을 넣어주세요. \n");
+        printf("이차항:");
+        scanf_s("%lf", &a);
+        if (a == 0)
+        {
+            printf("프로그램을 종료합니다.\n");
+            break;
+        }
+        printf("일차항:");
+        scanf_s("%lf", &b);
+        printf("상수항:");
+        scanf_s("%lf", &c);
 
-    discriminant = b * b - 4 * a * c;
+       
 
-    if (discriminant > 0)
-    {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
-        printf("실근 = %.2f,%.2f\n", root1, root2);
-    }
-    else if (discriminant == 0)
-    {
-        root1 = -b / (2 * a);
-        printf("One real root:\nRoot = %.2f\n", root1);
-    }
-    else
-    {
+        discriminant = b * b - 4 * a * c;
 
-        printf("허근입니다.");
-    }
+        if (discriminant > 0)
+        {
+            root1 = (-b + sqrt(discriminant)) / (2 * a);
+            root2 = (-b - sqrt(discriminant)) / (2 * a);
+            printf("실근 = %.2f,%.2f\n", root1, root2);
+        }
+        else if (discriminant == 0)
+        {
+            root1 = -b / (2 * a);
+            printf("중근 = %.2f\n", root1);
+        }
+        else
+        {
 
+            printf("허근입니다. \n");
+        }
+    }
     return 0;
 }
